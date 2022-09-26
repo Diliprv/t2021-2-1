e23bfe6d622a89e86822f13b985046b14ba336fc
Diliprv <114416665+Diliprv@users.noreply.github.com>
2022-09-26 20:55:06 +0530

    Create Program-3.java

---
+++ Program-3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+Public class NumberSeries2
+{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter the number");
+    int a = scan.nextInt();
+    if(a%2==0)
+    {
+      for(int i=1;i<=a-1;i++)
+      {
+      System.out.print(2*i-1+",");
+      }
+    }
+  else
+  {
+    for(int i=1;i<=a;i++)
+    {
+      System.out.print(2*i-1+",");
+    }
+  }
+  }
+}
