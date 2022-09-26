ea6df1435360bce4c00000a2cb56897bf65418de
Diliprv <114416665+Diliprv@users.noreply.github.com>
2022-09-26 20:59:51 +0530

    Update Program-3.java

--- Program-3.java
+++ Program-3.java
@@ -4,8 +4,10 @@ Public class NumberSeries2
   public static void main(String[] args)
   {
     Scanner scan = new Scanner(System.in);
+    // Entering the Number as an Input
     System.out.println("Enter the number");
     int a = scan.nextInt();
+    // LOgic to Print the Reqiured Output
     if(a%2==0)
     {
       for(int i=1;i<=a-1;i++)
